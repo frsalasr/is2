adbfa0e145d199d2974ac19d646dae177b2a553a
Francisco <frsalas@udec.cl>
2018-07-05 16:56:07 -0400

    cambios login/register

--- myapp/grupo4test/urls.py
+++ myapp/grupo4test/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
+
 
 from . import views
 
 urlpatterns = [
     #path('', views.base, name='base'),
     ## PAGINA DE INICIO
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
+    path('', auth_views.login, name='login'),
 
 	# registro de usuarios
     path('accounts/', include('django.contrib.auth.urls')),
